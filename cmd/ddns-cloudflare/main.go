e473a95c74e9fbd50da0678d917550b9960bdb37
Nenad Stojanovikj <me+github@nenad.dev>
2020-04-01 22:25:31 +0200

    Use config in main command

--- cmd/ddns-cloudflare/main.go
+++ cmd/ddns-cloudflare/main.go
@@ -1,53 +1,36 @@
 package main
 
 import (
-	"context"
 	"cloudflare-ddns/pkg/cloudflare"
+	"cloudflare-ddns/pkg/config"
 	"cloudflare-ddns/pkg/ip"
+	"context"
 	"fmt"
 	"os"
-	"strconv"
-	"time"
 )
 
 func main() {
-	token := os.Getenv("TOKEN")
-	if token == "" {
-		fail("Environment variable TOKEN is not set, exiting.")
-	}
-
-	domain := os.Getenv("DOMAIN")
-	if domain == "" {
-		fail("Environment variable DOMAIN is not set, exiting.")
-	}
-
-	version := os.Getenv("VERSION")
-	if version != "A" && version != "AAAA" {
-		fail("Environment variable VERSION must be 'A' for IPv4 or 'AAAA' for IPv6")
-	}
-	// TODO Abstract IP version in a separate package
-	// TODO Make ipify URL customizable
-	ipifyVersion := ip.V4
-	if version == "AAAA" {
-		ipifyVersion = ip.V6
-	}
-
-	secTimeout, _ := strconv.Atoi(os.Getenv("TIMEOUT"))
-	if secTimeout == 0 {
-		secTimeout = 10
-	}
-	timeout := time.Second * time.Duration(secTimeout)
-	proxied := os.Getenv("PROXIED") != "false"
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	cf, err := cloudflare.NewClient(token, cloudflare.Timeout(timeout), cloudflare.Retry(3))
+	cfg, err := config.FromEnvironment()
 	if err != nil {
 		fail(err)
 	}
 
-	rec, err := cf.GetRecord(ctx, domain, cloudflare.Type(version))
+	// TODO Abstract IP version in a separate package
+	// TODO Make ipify URL customizable
+	ipifyVersion := ip.V4
+	if cfg.Type == "AAAA" {
+		ipifyVersion = ip.V6
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cf, err := cloudflare.NewClient(cfg.Token, cloudflare.Timeout(cfg.Timeout), cloudflare.Retry(3))
+	if err != nil {
+		fail(err)
+	}
+
+	rec, err := cf.GetRecord(ctx, cfg.Domain, cloudflare.Type(cfg.Type))
 	if err != nil {
 		fail(err)
 	}
@@ -62,12 +45,12 @@ func main() {
 			Name:    rec.Name,
 			Type:    rec.Type,
 			Content: myIP,
-			Proxied: proxied,
+			Proxied: cfg.Proxied,
 		})
 		if err != nil {
 			fail("could not update record, got " + err.Error())
 		}
-		fmt.Printf("Updated %q to point from %s to %s\n", domain, rec.Content, myIP)
+		fmt.Printf("Updated %q to point from %s to %s\n", cfg.Domain, rec.Content, myIP)
 	} else {
 		fmt.Println("No updates")
 	}
